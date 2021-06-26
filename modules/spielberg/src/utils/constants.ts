9dfc70bccc077da5e5d5c4732bfd416cf2788ae4
WilsonCazarre <wcs0486@gmail.com>
2021-06-26 19:44:50 -0300

    feat(spielberg): api url in the run command

--- modules/spielberg/src/utils/constants.ts
+++ modules/spielberg/src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const __prod__ = process.env.NODE_ENV === "production";
-export const kubrickUrl = process.env.NEXT_PUBLIC_API_BASE_URL!;
+export const KUBRICK_URL = process.env.KUBRICK_URL!;
