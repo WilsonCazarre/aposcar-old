40e76cee8447f340fd21ca799d791071cfa925ad
WilsonCazarre <wcs0486@gmail.com>
2021-06-29 16:29:26 -0300

    feat(spielberg): register users

--- modules/spielberg/src/components/auth/RegisterForm.tsx
+++ modules/spielberg/src/components/auth/RegisterForm.tsx
@@ -3,18 +3,57 @@ import InputWithIcon from "../forms/InputWithIcon";
 import { UserIcon, LockClosedIcon, MailIcon } from "@heroicons/react/outline";
 import Button from "../Button";
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import { kubrick } from "../../lib/apiClient";
+import { AxiosError, AxiosResponse } from "axios";
+import { User } from "../../lib/apiEntities";
 
 interface Props {}
 
+interface FormFields {
+  username: string;
+  email: string;
+  password: string;
+}
+
 const RegisterForm: React.FC<Props> = () => {
   const router = useRouter();
-  return (
-    <form>
-      <div className="space-y-2">
-        <InputWithIcon HeroIcon={UserIcon} placeholder="username" />
+  const { register, handleSubmit } = useForm<FormFields>();
+  const registerMutation = useMutation<
+    AxiosResponse<User>,
+    AxiosError,
+    FormFields
+  >((payload) => kubrick.post("users/", payload), {
+    onSuccess: () => {
+      router.push("/login");
+    },
+  });
 
-        <InputWithIcon HeroIcon={MailIcon} placeholder="email" />
-        <InputWithIcon HeroIcon={LockClosedIcon} placeholder="password" />
+  const onSubmit = (data: FormFields) => {
+    registerMutation.mutate(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="space-y-2">
+        <InputWithIcon
+          HeroIcon={UserIcon}
+          placeholder="username"
+          {...register("username")}
+        />
+
+        <InputWithIcon
+          HeroIcon={MailIcon}
+          placeholder="email"
+          {...register("email")}
+        />
+        <InputWithIcon
+          HeroIcon={LockClosedIcon}
+          placeholder="password"
+          type="password"
+          {...register("password")}
+        />
       </div>
       <div className="grid grid-cols-2 gap-2 mt-16">
         <Button
