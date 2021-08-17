bfedadc05fcb7d5e3dd581087cfd0f5b4e22f315
WilsonCazarre <wcs0486@gmail.com>
2021-08-16 22:23:34 -0300

    wip: new layout

--- modules/spielberg/src/components/navigation/Navbar.tsx
+++ modules/spielberg/src/components/navigation/Navbar.tsx
@@ -6,10 +6,13 @@ import Link from "next/link";
 import useAuth from "../../utils/useAuth";
 import ProfileButton from "./ProfileButton";
 import LogoutButton from "./LogoutButton";
+import { useScreenType } from "../../utils/useScreenType";
 
 const Navbar: React.FC = () => {
   const router = useRouter();
-  const { loggedUser, logout } = useAuth();
+  const { loggedUser } = useAuth();
+  const screenType = useScreenType();
+  const showUserOnSidebar = screenType !== "3-cols";
 
   return (
     <nav className="px-10 py-3 flex justify-between items-center flex-none bg-gray-900">
@@ -18,18 +21,20 @@ const Navbar: React.FC = () => {
           <AposcarLogo />
         </a>
       </Link>
-      <div>
-        {loggedUser ? (
-          <div className="flex items-center space-x-4">
-            <LogoutButton />
-            <ProfileButton user={loggedUser} />
-          </div>
-        ) : (
-          <Button color="primary" onClick={() => router.push("/register")}>
-            Register
-          </Button>
-        )}
-      </div>
+      {showUserOnSidebar && (
+        <div>
+          {loggedUser ? (
+            <div className="flex items-center space-x-4">
+              <LogoutButton />
+              <ProfileButton user={loggedUser} />
+            </div>
+          ) : (
+            <Button color="primary" onClick={() => router.push("/register")}>
+              Register
+            </Button>
+          )}
+        </div>
+      )}
     </nav>
   );
 };
