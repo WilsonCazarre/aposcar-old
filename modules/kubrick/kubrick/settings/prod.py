385a7466833847980394bd21b649dbccaadb226e
WilsonCazarre <wcs0486@gmail.com>
2021-09-01 19:30:38 -0300

    feat(kubrick): new logging config

--- modules/kubrick/kubrick/settings/prod.py
+++ modules/kubrick/kubrick/settings/prod.py
@@ -55,19 +55,33 @@ CSRF_COOKIE_HTTPONLY = False
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(asctime)s [%(process)d] [%(levelname)s] "
+                + "pathname=%(pathname)s lineno=%(lineno)s "
+                + "funcname=%(funcName)s %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
     "handlers": {
-        "file": {
+        "null": {
             "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": "server.log",
+            "class": "logging.NullHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
     },
     "loggers": {
-        "django": {
-            "handlers": ["file"],
-            "level": "DEBUG",
-            "propagate": True,
-        },
+        "testlogger": {
+            "handlers": ["console"],
+            "level": "INFO",
+        }
     },
 }
 
